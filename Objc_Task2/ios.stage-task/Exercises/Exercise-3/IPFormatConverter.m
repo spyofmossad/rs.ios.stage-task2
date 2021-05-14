c077c90b8173b53723f1f19064cda79ec14c9505
Dzmitry Navitski <spyofmossad@gmail.com>
2021-05-14 16:03:01 +0300

    feat: add exercise3 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,38 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    if ([numbersArray count] == 0) {
+        return @"";
+    }
+    
+    NSMutableArray *ipArray = [numbersArray mutableCopy];
+    NSEnumerator *enumerator = [ipArray objectEnumerator];
+    NSNumber *value;
+    
+    for (int i = 0; i < 4; i++) {
+        value = [enumerator nextObject];
+        
+        if (!value) {
+            [ipArray addObject:@(0)];
+        }
+        
+        if ([value intValue] < 0) {
+            return @"Negative numbers are not valid for input.";
+        }
+        
+        if ([value intValue] > 255) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+        }
+    }
+    
+    NSString *ipAddress = [[NSString alloc] initWithFormat:@"%@.%@.%@.%@",
+                           [ipArray objectAtIndex:0],
+                           [ipArray objectAtIndex:1],
+                           [ipArray objectAtIndex:2],
+                           [ipArray objectAtIndex:3]];
+    
+    return ipAddress;
 }
 
 @end
