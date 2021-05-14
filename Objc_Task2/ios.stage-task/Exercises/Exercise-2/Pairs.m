ff709492b176c558dcdabca5697709fd57a276a8
Dzmitry Navitski <spyofmossad@gmail.com>
2021-05-14 14:03:40 +0300

    feat: add exercise2 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,19 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    NSInteger counter = 0;
+    NSArray <NSNumber *> *sortedArray = [[[array sortedArrayUsingSelector:@selector(compare:)] reverseObjectEnumerator] allObjects];
+    
+    for (int i = 0; i < [sortedArray count]; i++) {
+        for (int j = i + 1; j < [sortedArray count]; j++) {
+            if ([[sortedArray objectAtIndex:i] intValue] - [[sortedArray objectAtIndex:j] intValue] == [number intValue]) {
+                counter++;
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
