fc6d6abba3cc01e6589bd99da548f9df2d827b1d
Dzmitry Navitski <spyofmossad@gmail.com>
2021-05-14 21:13:57 +0300

    feat: add exercise4 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,42 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    if (!array) {
+        return @[];
+    }
+    
+    NSMutableArray *stringBuffer = [NSMutableArray new];
+    NSMutableArray *numbersBuffer = [NSMutableArray new];
+    
+    for (int i = 0; i < [array count]; i++) {
+        
+        if (![array[i] isKindOfClass:[NSArray class]]) {
+            return @[];
+        }
+        
+        for (int j = 0; j < [array[i] count]; j++) {
+            if ([array[i][j] isKindOfClass:[NSString class]]) {
+                [stringBuffer addObject: array[i][j]];
+            } else {
+                [numbersBuffer addObject:array[i][j]];
+            }
+            
+        }
+    }
+    
+    [stringBuffer sortUsingSelector:@selector(compare:)];
+    [numbersBuffer sortUsingSelector:@selector(compare:)];
+    
+    if ([stringBuffer count] == 0) {
+        return numbersBuffer;
+    }
+    
+    if ([numbersBuffer count] == 0) {
+        return stringBuffer;
+    }
+    
+    return [[NSArray alloc] initWithObjects:numbersBuffer, stringBuffer, nil];
 }
 
 @end
