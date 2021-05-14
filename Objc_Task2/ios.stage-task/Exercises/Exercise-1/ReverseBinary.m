e85d06556e9241163054e1f3d1c807b818651047
Dzmitry Navitski <spyofmossad@gmail.com>
2021-05-14 13:29:40 +0300

    feat: add exercise1 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,20 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+        
+    NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity: 8];
+    NSUInteger x = n;
+    
+    do {
+        [arr addObject:[NSString stringWithFormat: @"%lu", x&1]];
+    } while (x >>= 1);
+    
+    while ([arr count] < 8) {
+        [arr addObject:@"0"];
+    }
+    
+    NSString *reversedString = [[arr valueForKey:@"description"] componentsJoinedByString:@""];
+    long reversedValue = strtol([reversedString UTF8String], NULL, 2);
+    
+    return reversedValue;
 }
